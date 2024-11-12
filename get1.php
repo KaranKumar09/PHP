c60c8f65545b2c43fa926a4d37dfa77a07b23fff
Karan Kumar <118922288+KaranKumar09@users.noreply.github.com>
2024-11-12 15:17:04 +0530

    Add files via upload

---
+++ get1.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+Welcome <?php echo $_POST["username"];?> </br>
+Your blood group is: <?php echo $_POST["bloodgroup"]; ?></br>
+Your age is:  <?php echo $_POST["age"];?> </br>
+Your address:  <?php echo $_POST["address"];?>
+</body>
+</html>
