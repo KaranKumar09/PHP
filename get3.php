0752acc0c87fff0adfcd098d2484a756aa951025
Karan Kumar <118922288+KaranKumar09@users.noreply.github.com>
2024-11-14 11:30:06 +0530

    Add files via upload

---
+++ get3.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <pre>
+            <?php
+            echo $_SERVER['PHP_SELF'];
+            echo "<br>";
+            echo "<br>";
+            echo $_SERVER['SERVER_NAME'];
+            echo "<br>";
+            echo "<br>";
+            echo $_SERVER['HTTP_HOST'];
+            echo "<br>";
+            echo "<br>";
+            echo $_SERVER['SCRIPT_NAME'];
+            ?>
+        </pre>
+    </body>
+</html>
